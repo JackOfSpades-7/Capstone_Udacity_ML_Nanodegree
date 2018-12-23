cc56214c467d163f6f53f6d7ee887bb134885c13
Johnathan Widney <35155088+JackOfSpades-7@users.noreply.github.com>
2018-12-22 16:46:14 -0800

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,100 @@
+print("hello world")
+
+print("my name")
+
+print("42")
+
+print("is this the real life?")
+
+print("or is it just a fantasy?")
+
+print("qunatum computing")
+
+print("Mars")
+
+print("Earth")
+
+print("Curiosity Rover")
+
+print("let's do this")
+
+3 + 4 
+
+7 + 2
+
+9 + 1
+
+0 + 8
+
+12 + 5
+
+10 + 7
+
+179 + 0
+
+4 + 2
+
+100 + 27
+
+42 + 8
+
+if 0 is 0:
+    print("nothing is nothing")
+    
+if 3 + 7 and 2 + 8 == 10:
+    print("10")
+
+if True == True:
+    print("True")
+    
+if False is False:
+    print("False")
+    
+if 0 == 1 is False:
+    print("0 /= 1")
+    
+if 2 == 7:
+    print("?")
+else:
+    print("nope")
+        
+if True is False:
+    print("error")
+else:
+    print("lies")
+        
+if "you and I" == "us and them":
+    print("error")
+else:
+    print("just the 2 of us")
+        
+if "him" == "that guy":
+    print("yes, that guy.")
+else:
+    print("not him")
+        
+if "this" == "that":
+    print("what?")
+else:
+    print("these are not the same")
+        
+7 - 5
+
+2 - 1
+
+0 - 5
+
+9 - 7
+
+4 - 1
+
+8 - 3
+
+9 - 2
+
+10 - 9
+
+179 - 79
+
+42 - 7
+
